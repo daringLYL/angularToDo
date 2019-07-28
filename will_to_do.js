1b9d1e978dc96114858ca3dee334d6cab9ca0689
daringLYL <1032982127@qq.com>
2019-07-28 19:00:17 +0800

    添加“恢复”功能

--- will_to_do.js
+++ will_to_do.js
@@ -1,7 +1,8 @@
-const app = angular.module('exercise', ['ngStorage']);
-app.controller('doExercise', function ($scope,$localStorage) {
-    $scope.getThing = '';
-    $scope.search = '';
+var app = angular.module("exercise", ['ngStorage']);
+app.controller("doExercise", function ($scope,$localStorage) {
+    $scope.getThing = "";
+    $scope.search = "";
+    $scope.recover = [];
     $scope.$storage = $localStorage.$default({
        result :[]
     });
@@ -19,16 +20,39 @@ app.controller('doExercise', function ($scope,$localStorage) {
             $scope.show();
         }
     }
+<<<<<<< HEAD
     $scope.delete = function(item){
         if(confirm('是否确认'+item+'删除？')){
             for (index in $scope.$storage.result) {
                 if(item === $scope.$storage.result[index]){
+=======
+    $scope.$storage = $localStorage.$default({
+        recover :[]
+     });
+    $scope.delete = function(item){
+        if(confirm("是否确认"+item+"删除？")){
+            for (index in $scope.$storage.result) {
+                if(item == $scope.$storage.result[index]){
+                    $scope.$storage.recover.push({'name':item.name,'date':item.date});
+>>>>>>> 添加“恢复”功能
                     $scope.$storage.result.splice(index,1);
                 }
             }
         }
     };
-
-    
+    $scope.deleteRecover = function(item){
+        if(confirm("是否确认"+item+"删除？")){
+            for (index in $scope.$storage.recover) {
+                if(item == $scope.$storage.recover[index]){
+                    $scope.$storage.recover.splice(index,1);
+                }
+            }
+        }
+    };
+    $scope.show=true;
+        $scope.showRecover=function () {
+          $scope.show=!$scope.show;
+          console.log($scope.show);
+        }
            })
 
