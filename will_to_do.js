8536e889a6848650c8ef00e512dbe023827df9e8
daringLYL <1032982127@qq.com>
2019-07-29 22:25:06 +0800

    已经按照最新的评论修改

--- will_to_do.js
+++ will_to_do.js
@@ -1,58 +1,45 @@
-var app = angular.module("exercise", ['ngStorage']);
-app.controller("doExercise", function ($scope,$localStorage) {
+const app = angular.module("exercise", ['ngStorage']);
+app.controller("doExercise", function ($scope, $localStorage) {
     $scope.getThing = "";
     $scope.search = "";
     $scope.recover = [];
     $scope.$storage = $localStorage.$default({
-       result :[]
+        result: []
     });
     $scope.selected = false;
     $scope.show = function () {
-        $scope.$storage.result.push({'name':$scope.getThing,'date':new Date().toLocaleDateString()});
+        $scope.$storage.result.push({ 'name': $scope.getThing, 'date': new Date().toLocaleDateString() });
         $scope.getThing = '';
     }
     $scope.done = function (index) {
         $scope.selected = !$scope.selected;
     }
     $scope.enterEvent = function (e) {
-        var keycode = window.event ? e.keyCode : e.which;
+        const keycode = window.event ? e.keyCode : e.which;
         if (keycode === 13) {
             $scope.show();
         }
     }
-<<<<<<< HEAD
-    $scope.delete = function(item){
-        if(confirm('是否确认'+item+'删除？')){
-            for (index in $scope.$storage.result) {
-                if(item === $scope.$storage.result[index]){
-=======
     $scope.$storage = $localStorage.$default({
-        recover :[]
-     });
-    $scope.delete = function(item){
-        if(confirm("是否确认"+item+"删除？")){
-            for (index in $scope.$storage.result) {
-                if(item == $scope.$storage.result[index]){
-                    $scope.$storage.recover.push({'name':item.name,'date':item.date});
->>>>>>> 添加“恢复”功能
-                    $scope.$storage.result.splice(index,1);
-                }
-            }
+        recover: []
+    });
+    $scope.delete = function (item, index) {
+        if(confirm(`是否确认${item}删除`)){
+        if (item == $scope.$storage.result[index]) {
+            $scope.$storage.recover.push(item);
+            $scope.$storage.result.splice(index, 1);
         }
+    }};
+    $scope.deleteRecover = function (item, index) {
+        if(confirm(`是否确认${item}删除`)){
+        if (item == $scope.$storage.recover[index]) {
+            $scope.$storage.recover.splice(index, 1);
+        }
+    }
     };
-    $scope.deleteRecover = function(item){
-        if(confirm("是否确认"+item+"删除？")){
-            for (index in $scope.$storage.recover) {
-                if(item == $scope.$storage.recover[index]){
-                    $scope.$storage.recover.splice(index,1);
-                }
-            }
-        }
-    };
-    $scope.show=true;
-        $scope.showRecover=function () {
-          $scope.show=!$scope.show;
-          console.log($scope.show);
-        }
-           })
+    $scope.onShow = true;
+    $scope.showRecover = function () {
+        $scope.onShow = !$scope.onShow;
+    }
+})
 
